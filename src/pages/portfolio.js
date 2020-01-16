08460bb164f24130d7cab6eeb3c0a5a686dadee8
Hal <hal.shin@alumni.ubc.ca>
2020-01-16 01:35:20 -0800

    New button styles and added pomodoro clock to portfolio

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -7,7 +7,27 @@ import SEO from "../components/seo"
 const AboutPage = () => (
   <Layout>
     <SEO title="Portfolio" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Portfolio</h1>
+    <h1>Projects</h1>
+    <hr />
+    <h2>Pomodoro Clock</h2>
+    <p>
+      A pomodoro clock is intended to facilitate the{" "}
+      <a
+        href="https://en.wikipedia.org/wiki/Pomodoro_Technique"
+        target="_blank"
+      >
+        pomodoro technique
+      </a>{" "}
+      by presetting a timer to 25 minutes, which by default leads to a 5 minute
+      break period.
+    </p>
+    <p>
+      This app was created using <strong>HTML</strong>, <strong>CSS</strong>,
+      and <strong>plain JavaScript</strong>.
+    </p>
+    <a href="https://communitybicycle.github.io/pomodoro-clock/">
+      <button>Go to Project</button>
+    </a>
     <hr />
     <h2>JavaScript Calculator</h2>
     <p>
