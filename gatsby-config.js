c22cf2f740e8b02d29c636f838aade97e40b6c29
Hal Shin <hal.shin@alumni.ubc.ca>
2020-08-15 19:04:54 -0700

    Add google analytics tracking code

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,6 +65,12 @@ module.exports = {
         icon: `src/images/favicon-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-106214224-3",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
